c044e8333efd4257ff84b89fc537384df59ed9ce
antlypls <antlypls@gmail.com>
2010-03-09 19:34:19 +0400

    add unit tests draft

---
+++ test/rumeme_test.rb
@@ -0,0 +1,28 @@
+#units test for rumeme
+
+class RumemeTest < Test::Unit::TestCase
+  def test_parse_reply_with_id
+    result = SmsReply.parse("12 79270123456 100 asdfgh", true)
+    assert 12, result.message_id
+    assert '79270123456', result.phone_number
+    assert 100, result.when
+    assert 'asdfgh', result.message
+    assert MessageStatus::NONE, result.status
+  end
+
+  def parse_reply_without_id
+    result = SmsReply.parse("79270123456 100 asdfgh", false)
+    assert_nil result.message_id
+    assert '79270123456', result.phone_number
+    assert 100, result.when
+    assert 'asdfgh', result.message
+    assert MessageStatus::NONE, result.status
+  end
+
+  def parse_delivery_report_pending
+    result = SmsReply.parse("12 1 100", true)
+    assert 12, result.message_id
+    assert MessageStatus::PENDING, result.status
+    assert 100, result.when
+  end
+end
