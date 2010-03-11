7c0cce84876c187aa8c4619f999c02cc53edcdc6
antlypls <antlypls@gmail.com>
2010-03-11 14:39:00 +0400

    change SmsMessage class constructor parameters to hash

--- lib/rumeme/configuration.rb
+++ lib/rumeme/configuration.rb
@@ -4,7 +4,6 @@ module Rumeme
     attr_accessor :password
     attr_accessor :use_message_id
     attr_accessor :secure
-    #attr_accessor :proxy_settings
     attr_accessor :allow_splitting
     attr_accessor :allow_long_messages
   end
--- lib/rumeme/sms_message.rb
+++ lib/rumeme/sms_message.rb
@@ -4,9 +4,15 @@ module Rumeme
     attr_reader :phone_number, :message, :message_id, :delay, :validity_period, :delivery_report
 
     # Constructor.
-    def initialize phone_number, message, message_id, delay, validity_period, delivery_report
-      @phone_number, @message, @message_id, @delay, @validity_period, @delivery_report = phone_number, message, message_id, delay, validity_period, delivery_report
-      @message = message.gsub("\n",'\n').gsub("\r",'\r').gsub("\\",'\\\\')
+    def initialize args
+      defaults = {:phone_number => nil, :message => nil, :message_id => 0, :delay => 0, :validity_period => ValidityPeriod::THREE_DAYS, :delivery_report => false}
+      params = defaults.merge args
+      defaults.keys.each {|k| instance_variable_set("@#{k.to_s}".to_sym, params[k])}
+
+      raise ArgumentError.new("phone_number is nil") if @phone_number.nil?
+      raise ArgumentError.new("phone_number is empty") if @phone_number.nil? || @phone_number.empty?
+
+      @message = @message.gsub("\n",'\n').gsub("\r",'\r').gsub("\\",'\\\\')
     end
   end
 end
